ff81efe851aa2cffe0ffe361de6a7c7f1acfa39e
sukhomlyn-pn-lab <sukhomlyn.pn@ucu.edu.ua>
2026-05-07 20:06:11 +0300

    "Little changes in constr.py"

--- Constructor binary tree/constr.py
+++ Constructor binary tree/constr.py
@@ -1,12 +1,12 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+    def buildTree(self, preorder: list[int], inorder: list[int]) -> TreeNode:
         # Create a map to efficiently find the index of any element in inorder list
         io_map = {}
         for i in range(len(inorder)):
