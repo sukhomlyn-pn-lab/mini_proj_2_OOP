2608d947c6e6ffcb4b717883f50f0245e3884bac
sukhomlyn-pn-lab <sukhomlyn.pn@ucu.edu.ua>
2026-05-07 20:42:43 +0300

    "Change reverse tree"

--- reverse_tree/reverse_tree.py
+++ reverse_tree/reverse_tree.py
@@ -8,9 +8,9 @@ class TreeNode:
 
 
 def invertTree(self, root: TreeNode) -> TreeNode:
-        if root is None:
-            return None
-        root.left, root.right = root.right, root.left
-        self.invertTree(root.left)
-        self.invertTree(root.right)
-        return root
+    if root is None:
+        return None
+    root.left, root.right = root.right, root.left
+    self.invertTree(root.left)
+    self.invertTree(root.right)
+    return root
