8f3b3574019cd8b1906876cf05a297b171d2cd24
sukhomlyn-pn-lab <sukhomlyn.pn@ucu.edu.ua>
2026-05-07 20:41:34 +0300

    Task "Combination Sum"

--- Binary Tree Maximum Path Sum/max_path.py
+++ Binary Tree Maximum Path Sum/max_path.py
@@ -1,12 +1,12 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+    def maxPathSum(self, root: TreeNode) -> int:
         max_sum = float('-inf')
 
         def dfs(node):
