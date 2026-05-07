d46123f0125b69c787e09893d283862802422508
sukhomlyn-pn-lab <sukhomlyn.pn@ucu.edu.ua>
2026-05-07 12:26:30 +0300

    Done task "Reverse tree"

---
+++ reverse_tree/reverse_tree.py
@@ -0,0 +1,16 @@
+"""Reverse Tree"""
+
+class TreeNode:
+    def __init__(self, value, left:None, right:None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def invertTree(self, root: TreeNode) -> TreeNode:
+        if root is None:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
